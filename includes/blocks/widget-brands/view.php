553ab436ab54efe154a22fec29d649ebdcff4c1f
Sebastian Dollschneider <sebastian@dollschneider.net>
2023-03-03 02:02:53 +0100

    fix footer widget block's max width

--- includes/blocks/widget-brands/view.php
+++ includes/blocks/widget-brands/view.php
@@ -1,22 +1,26 @@
 <div id="<?= $block_id ?>" class="<?= $block_classes ?>">
 
-    <div class="block__inner">
+    <div class="block__width">
 
-        <div class="block__row">
+        <div class="block__inner">
 
-			<?php if ( ! empty( $text ) ) : ?>
-                <div class="brands__text">
-					<?= $text ?>
-                </div>
-			<?php endif; ?>
+            <div class="block__row">
 
-			<?php if ( ! empty( $logos ) ) : ?>
-                <div class="brands__items">
-					<?php foreach ( $logos as $logo ): ?>
-                        <?= $logo ?>
-					<?php endforeach; ?>
-                </div>
-			<?php endif; ?>
+				<?php if ( ! empty( $text ) ) : ?>
+                    <div class="brands__text">
+						<?= $text ?>
+                    </div>
+				<?php endif; ?>
+
+				<?php if ( ! empty( $logos ) ) : ?>
+                    <div class="brands__items">
+						<?php foreach ( $logos as $logo ): ?>
+							<?= $logo ?>
+						<?php endforeach; ?>
+                    </div>
+				<?php endif; ?>
+
+            </div>
 
         </div>
 
