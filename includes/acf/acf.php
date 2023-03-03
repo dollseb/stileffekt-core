aa856aa0152964e1d167529a63d9499183e986d9
Sebastian Dollschneider <sebastian@dollschneider.net>
2023-03-03 02:18:19 +0100

    add acf users

--- includes/acf/acf.php
+++ includes/acf/acf.php
@@ -14,10 +14,12 @@ if ( file_exists( get_theme_file_path( '/includes/acf/extensions/field-menus.php
  * acf backend visibility
  *
  */
-function stileffekt__acf_visibility()
+function whitelabel__acf_visibility()
 {
 	$users = array(
 		'sebastian@dollschneider.net',
+		'christoph@stileffekt.de',
+		'wiem@stileffekt.de',
 	);
 
 	$current_user = wp_get_current_user();
@@ -29,4 +31,4 @@ function stileffekt__acf_visibility()
 	return false;
 }
 
-add_filter('acf/settings/show_admin', 'stileffekt__acf_visibility');
+add_filter('acf/settings/show_admin', 'whitelabel__acf_visibility');
